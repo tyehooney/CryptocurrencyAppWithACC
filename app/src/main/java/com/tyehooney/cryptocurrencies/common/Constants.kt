98a0b426e4af493a382e60d2683668f238364480
tyehooney <xogns4313@naver.com>
2021-12-08 22:06:07 +0900

    feat : MainActivity 설정

--- app/src/main/java/com/tyehooney/cryptocurrencies/common/Constants.kt
+++ app/src/main/java/com/tyehooney/cryptocurrencies/common/Constants.kt
@@ -7,4 +7,7 @@ object Constants {
     const val PARAM_COIN_ID = "coinId"
 
     const val UNEXPECTED_ERROR_MSG = "An unexpected error occured"
+
+    const val COIN_LIST_SCREEN = "coin_list_screen"
+    const val COIN_DETAIL_SCREEN = "coin_detail_screen"
 }
--- app/src/main/java/com/tyehooney/cryptocurrencies/presentation/MainActivity.kt
+++ app/src/main/java/com/tyehooney/cryptocurrencies/presentation/MainActivity.kt
@@ -2,11 +2,42 @@ package com.tyehooney.cryptocurrencies.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.tyehooney.cryptocurrencies.R
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.tyehooney.cryptocurrencies.presentation.coin_detail.CoinDetailScreen
+import com.tyehooney.cryptocurrencies.presentation.coin_list.CoinListScreen
+import com.tyehooney.cryptocurrencies.presentation.ui.theme.CryptocurrencyAppYTTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            CryptocurrencyAppYTTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinListScreen.route
+                    ) {
+                        composable(
+                            route = Screen.CoinListScreen.route
+                        ) {
+                            CoinListScreen(navController)
+                        }
+                        composable(
+                            route = Screen.CoinDetailScreen.route + "/{coinId}"
+                        ) {
+                            CoinDetailScreen()
+                        }
+                    }
+                }
+            }
+        }
     }
 }
