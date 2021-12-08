20633daadda3841932b334fe00acb1477756b91e
tyehooney <xogns4313@naver.com>
2021-12-08 18:51:31 +0900

    feat: Coin, CoinDetail dto 생성

---
+++ app/src/main/java/com/tyehooney/cryptocurrencies/data/remote/dto/TeamMember.kt
@@ -0,0 +1,7 @@
+package com.tyehooney.cryptocurrencies.data.remote.dto
+
+data class TeamMember(
+    val id: String,
+    val name: String,
+    val position: String
+)
---
+++ app/src/main/java/com/tyehooney/cryptocurrencies/data/remote/dto/Whitepaper.kt
@@ -0,0 +1,6 @@
+package com.tyehooney.cryptocurrencies.data.remote.dto
+
+data class Whitepaper(
+    val link: String,
+    val thumbnail: String
+)
