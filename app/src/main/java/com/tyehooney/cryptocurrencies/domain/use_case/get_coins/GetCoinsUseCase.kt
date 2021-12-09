40b4889fa8c2f01a797fb7d6083868bba75afde3
tyehooney <xogns4313@naver.com>
2021-12-09 21:13:57 +0900

    feat : coin 리스트 조회시 캐싱 적용

--- app/src/main/java/com/tyehooney/cryptocurrencies/domain/use_case/get_coins/GetCoinsUseCase.kt
+++ app/src/main/java/com/tyehooney/cryptocurrencies/domain/use_case/get_coins/GetCoinsUseCase.kt
@@ -2,27 +2,50 @@ package com.tyehooney.cryptocurrencies.domain.use_case.get_coins
 
 import com.tyehooney.cryptocurrencies.common.Constants.UNEXPECTED_ERROR_MSG
 import com.tyehooney.cryptocurrencies.common.Resource
+import com.tyehooney.cryptocurrencies.data.local.toCoin
+import com.tyehooney.cryptocurrencies.data.remote.dto.CoinDto
 import com.tyehooney.cryptocurrencies.data.remote.dto.toCoin
 import com.tyehooney.cryptocurrencies.domain.model.Coin
-import com.tyehooney.cryptocurrencies.domain.repository.CoinRepository
+import com.tyehooney.cryptocurrencies.domain.repository.LocalCoinRepository
+import com.tyehooney.cryptocurrencies.domain.repository.RemoteCoinRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
 import java.io.IOException
+import java.net.UnknownHostException
 import javax.inject.Inject
 
 class GetCoinsUseCase @Inject constructor(
-    private val repository: CoinRepository
+    private val remoteRepository: RemoteCoinRepository,
+    private val localRepository: LocalCoinRepository
 ) {
     operator fun invoke(): Flow<Resource<List<Coin>>> = flow {
         try {
             emit(Resource.Loading<List<Coin>>())
-            val coins = repository.getCoins().map { it.toCoin() }
-            emit(Resource.Success<List<Coin>>(coins))
+            val coins = remoteRepository.getCoins()
+            emit(Resource.Success<List<Coin>>(coins.map { it.toCoin() }))
+            coins.forEach { coin -> saveCoin(coin) }
         } catch (e: HttpException) {
             emit(Resource.Error<List<Coin>>(e.localizedMessage ?: UNEXPECTED_ERROR_MSG))
         } catch (e: IOException) {
-            emit(Resource.Error<List<Coin>>(UNEXPECTED_ERROR_MSG))
+            when (e) {
+                is UnknownHostException -> emit(getCoinsFromDB())
+                else -> emit(Resource.Error<List<Coin>>(UNEXPECTED_ERROR_MSG))
+            }
         }
+    }
+
+    private suspend fun getCoinsFromDB(): Resource<List<Coin>> {
+        return try {
+            val coins = localRepository.getCoins().map { it.toCoin() }
+            if (coins.isNullOrEmpty()) Resource.Error<List<Coin>>(UNEXPECTED_ERROR_MSG)
+            else Resource.Success<List<Coin>>(coins)
+        } catch (e: Exception) {
+            Resource.Error<List<Coin>>(UNEXPECTED_ERROR_MSG)
+        }
+    }
+
+    private suspend fun saveCoin(coinDto: CoinDto) {
+        localRepository.saveCoin(coinDto)
     }
 }
